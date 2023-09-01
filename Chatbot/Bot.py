a381df1175673866a89cf3fc2c030c991568687c
FatimahAdwan <78910118+FatimahAdwan@users.noreply.github.com>
2023-09-02 00:52:05 +0100

    Add files via upload

---
+++ Chatbot/Bot.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a pythonista"}]
+
+def CustomChatGPT(Type):
+    messages.append({"role": "user", "content": Type})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+Web = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+
+Web.launch(share=True)
---
+++ Chatbot/chat.py
@@ -0,0 +1,4 @@
+
+import openai
+openai.api_key = "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"
+
